312c2e62d8ef16b0cccbc28c24e6e01e85f4bf0f
BlackBettyNB <JBNB1@hotmail.com>
2026-06-19 07:52:21 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'clock-tracker-v1';
+
+const URLS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+// Install: cache everything on first load
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate: clear out old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch: serve from cache, fall back to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
+  );
+});
