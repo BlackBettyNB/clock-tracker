a005db6868209976e952e4f69c089fe2f5a3bf85
BlackBettyNB <JBNB1@hotmail.com>
2026-09-13 09:21:05 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,5 @@
-var CACHE_NAME = 'clock-tracker-v4';
-
-var URLS_TO_CACHE = [
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) { return cache.addAll(URLS_TO_CACHE); })
-      .then(function() { return self.skipWaiting(); })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
-    }).then(function() { return self.clients.claim(); })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(cached) { return cached || fetch(event.request); })
-  );
-});
+var CACHE_NAME = 'clock-tracker-v7';
+var URLS_TO_CACHE = ['./index.html','./manifest.json'];
+self.addEventListener('install', function(event){event.waitUntil(caches.open(CACHE_NAME).then(function(cache){return cache.addAll(URLS_TO_CACHE);}).then(function(){return self.skipWaiting();}));});
+self.addEventListener('activate', function(event){event.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE_NAME;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
+self.addEventListener('fetch', function(event){event.respondWith(caches.match(event.request).then(function(cached){return cached||fetch(event.request);}));});
